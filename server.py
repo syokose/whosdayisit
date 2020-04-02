98cb197a293e50dbd618d5332c66725225b480ed
Sachi Yokose <sachi@Andys-MacBook-Pro-2.local>
2020-04-01 21:48:59 -0700

    added basic day detection functionality

--- server.py
+++ server.py
@@ -1,6 +1,20 @@
 from flask import Flask
+from datetime import date
+
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
-    return "Hi Andy - who's day is it?"
+def home():
+	today = date.today()
+	reference_day = date(2020, 3, 28)
+	delta = today - reference_day
+	even = delta.days % 2 == 0
+	person = "Andy" if even else "Sachi"
+	return "Today is " + person + "'s day! Have a good one sucker!"
+
+
+
+@app.route('/test')
+def test():
+    return "this is the test route"
