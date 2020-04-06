cebda1df5bda0535a2e930820c298f11cdab7f7f
Sachi Yokose <sachi.yokose@gmail.com>
2020-04-05 21:24:37 -0700

    updated forms to WTForms

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, flash, url_for
 from flask_sqlalchemy import SQLAlchemy
-from datetime import date
+from datetime import date, timedelta
 from dotenv import load_dotenv
+from wtforms import Form, IntegerField, TextAreaField, validators
 
 load_dotenv('.env.dev')
 
@@ -12,6 +13,7 @@ POSTGRES_PW = os.getenv("POSTGRES_PW")
 POSTGRES_DB = os.getenv("POSTGRES_DB")
 
 app = Flask(__name__)
+app.secret_key = 'lovebirds'
 
 DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
 app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
@@ -26,26 +28,81 @@ class Rating(db.Model):
     attitude_score = db.Column(db.Integer)
     cleanliness_score = db.Column(db.Integer)
     taste_score = db.Column(db.Integer)
+    comment = db.Column(db.String)
 
-@app.route('/')
+    def __repr__(self):
+        return 'Rating(subect='+self.subject+', day='+str(self.day)+ ')'
+
+class RatingForm(Form):
+    attitude_score = IntegerField('Attitude', [validators.NumberRange(min=0, max=10)])
+    cleanliness_score = IntegerField('Cleanliness', [validators.NumberRange(min=0, max=10)])
+    taste_score = IntegerField('Taste', [validators.NumberRange(min=0, max=10)])
+    comment = TextAreaField('Comment', [validators.Length(max=1000)])
+
+@app.route('/', methods = ['GET', 'POST'])
 def home():
+    form = RatingForm(request.form)
+    if request.method == 'POST' and form.validate():
+        db.session.add(Rating(subject=get_other_person(),
+                              day=get_yesterday(),
+                              attitude_score=form.attitude_score.data, 
+                              cleanliness_score=form.cleanliness_score.data, 
+                              taste_score=form.taste_score.data, 
+                              comment=form.comment.data))
+        db.session.commit()
+        flash('Thanks for helping us improve!')
+        return redirect(url_for('home'))
+    person = get_person()
+    other_person = get_other_person()
+    return render_template('home.html', name=person, other_name=other_person, form=form)
+
+def get_even():
     today = date.today()
     reference_day = date(2020, 3, 28)
     delta = today - reference_day
     even = delta.days % 2 == 0
+    return even
+
+def get_person():
+    even = get_even()
     person = "Andy" if even else "Sachi"
+    return person
+
+def get_other_person():
+    even = get_even()
     other_person = "Sachi" if even else "Andy"
-    return render_template('home.html', name=person, other_name=other_person)
+    return other_person
 
-@app.route('/template')
-def template():
-    return render_template('home.html', name='afdjkladsfds')
+def get_yesterday():
+    today = date.today()
+    yesterday = today - timedelta(days=1)
+    return yesterday
 
-@app.route('/test')
-def test():
-    return "this is the test route"
+@app.route('/', methods=['POST'])
+def handle_data():
+    print("form submit")
+    attitude_score = request.form['attitude_score']
+    cleanliness_score = request.form['cleanliness_score']
+    taste_score = request.form['taste_score']
+    comment=request.form['comment']
+    print(attitude_score, cleanliness_score, taste_score, comment)
+    db.session.add(Rating(subject=get_other_person(), day=get_yesterday(),attitude_score=attitude_score, cleanliness_score=cleanliness_score, taste_score=taste_score, comment=comment))
+    db.session.commit()
+    return home()
+
+@app.route('/test_read')
+def test_read():
+    result = str(Rating.query.all())
+    return result
+
 
 if __name__ == '__main__':
     app.jinja_env.auto_reload = True
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.run(debug=True, host='0.0.0.0')
+
+
+
+
+
+
